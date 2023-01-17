468b5300210c2978e7f54b3be16899a27134920a
Aman Kumar <Dr.AmanNavodian@gmail.com>
2023-01-17 09:40:35 +0530

    Permutation of Sring Done

---
+++ Permutations of a String.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void permutation(string &str, int start, int end){
+    if(start==end){
+        cout<<str<<endl;
+    }
+    for(int i = start; i < end; i++){
+        swap(str[i], str[start]);
+        permutation(str, start+1, end);
+        
+    }
+}
+
+int main(){
+    string str = "ABC";
+    int n = str.length();
+    permutation(str, 0, n);
+}
