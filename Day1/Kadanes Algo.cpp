66faa0152fa9b6d2d32fe7717185cecd250a454e
Aman Kumar <Dr.AmanNavodian@gmail.com>
2023-01-17 10:06:41 +0530

    Kadane's Algo Done

--- Day1/Kadanes Algo.cpp
+++ Day1/Kadanes Algo.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main(){
     int arr[10] = {-2,1,-3,4,-1,2,1,-5,4};
     int n = sizeof(arr)/ sizeof(arr[0]);
-    int mstn = INT_MIN;
+    int mstn = INT_MIN; //mstn = Maximum sum till Now.
     int sum = 0;
     for(int i = 0; i< n; i++){
         sum += arr[i];
         if(sum> mstn) mstn = sum;
         if(sum < 0) sum = 0;
     }
-    cout<<"Sum is "<<mstn;
+    cout<<"Maximum contiguous sum is "<<mstn<<endl;
 }
