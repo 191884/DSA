ddb16e70d686fdd62e98a92b2c981db58a8b1e3d
Aman Kumar <Dr.AmanNavodian@gmail.com>
2023-01-24 18:35:51 +0530

    Find Min & Max in Array Done.

--- Mastering Array/Find minimum and maximum element in an array.cpp
+++ Mastering Array/Find minimum and maximum element in an array.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 pair<long long, long long> getMinMax(long long a[], int n) {
     int low = INT_MAX, high= INT_MIN;
     for(int i = 0; i< n; i++){
@@ -12,4 +15,12 @@ pair<long long, long long> getMinMax(long long a[], int n) {
     ans.first = low;
     ans.second = high;
     return ans;
+}
+
+int main(){
+    long long a[] = { 4,5,3,2,1,5,6,4};
+    int n = sizeof(a)/sizeof(a[0]);
+    pair<int, int>  ans = getMinMax(a,n);
+    cout<<"Max element is: "<<ans.second<<endl;
+    cout<< "Min element is: "<<ans.first;
 }
