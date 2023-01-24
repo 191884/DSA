b60ca14b52456dc10b70dffc43f0bb63e1a4e30f
Aman Kumar <Dr.AmanNavodian@gmail.com>
2023-01-24 06:39:56 +0530

    Bitonic Point Using Binary Search

--- Mastering Array/Bitonic Point.cpp
+++ Mastering Array/Bitonic Point.cpp
@@ -14,3 +14,25 @@ public:
 	}
 };
 
+//Using Binary Search
+
+class Solution{
+public:
+	
+	int findMaximum(int arr[], int n) {
+	    int ans = findMaximumE(arr, 0, n, n);
+	    return ans;
+	}
+	
+	int findMaximumE(int arr[], int low, int high, int n) {
+	    int mid = low + (high - low)/2;
+	    if((mid==0 ||arr[mid] > arr[mid-1]) && (arr[mid] > arr[mid+1]|| mid ==n-1)){
+	        return arr[mid];
+	    }else{
+	         if(arr[mid]<arr[mid-1]&&mid>0){
+	            return findMaximumE(arr, 0, (mid-1), n);
+	    }   else 
+	            return findMaximumE(arr, (mid+1), high, n);
+	    }
+	}
+};
