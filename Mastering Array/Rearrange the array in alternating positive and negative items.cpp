5afeca7127950b03ac73d2c45566f74f07bfb539
Aman Kumar <Dr.AmanNavodian@gmail.com>
2023-01-29 21:07:32 +0530

    Rearranged but error

--- Mastering Array/Rearrange the array in alternating positive and negative items.cpp
+++ Mastering Array/Rearrange the array in alternating positive and negative items.cpp
@@ -7,13 +7,33 @@ void printArray(int arr[], int n){
     }
 }
 
+void rightRotate(int arr[], int n, int outOfPlace, int cur){
+    char temp = arr[cur];
+    for(int i = cur; i > outOfPlace; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[outOfPlace] =temp;
+}
+
 void rearrange(int arr[], int n){
-    // int outOfPlace = -1;
-    // for(int i = 0; i< n; i++){
-    //     if(outOfPlace >= 0 ){
-    //         if(arr[i] )
-    //     }
-    // }
+    int outOfPlace = -1;
+    for(int i = 0; i< n; i++){
+        if(outOfPlace >= 0 ){
+            if(((arr[i] >= 0) && (arr[outOfPlace] <0))|| ((arr[i] < 0)&&(arr[outOfPlace]>= 0))){
+                rightRotate(arr, n, outOfPlace, i);
+            }
+
+            if(i - outOfPlace >=2){
+                outOfPlace +=2;
+            }else{
+                outOfPlace = -1;
+            }
+        }
+        if (((arr[i] >= 0) && (!(i & 0x01)))
+                || ((arr[i] < 0) && (i & 0x01))) {
+                outOfPlace = i;
+            }
+    }
 }
 int main(){
     int arr[]= { -5, -2, 5, 2, 4, 7, 1, 8, 0, -8 };
@@ -23,4 +43,5 @@ int main(){
 
     cout<<"\nAfter rearranging Array is :";
     rearrange(arr,n);
+    printArray(arr, n);
 }
