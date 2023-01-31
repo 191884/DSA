0514f5daa17ff1213f2cc8f39522d31b30b5145c
Aman Kumar <dr.amannavodian@gmail.com>
2023-01-31 21:27:45 +0530

    maximum Product Subarray Done.

--- Mastering Array/Maximum Product Subarray.cpp
+++ Mastering Array/Maximum Product Subarray.cpp
@@ -2,36 +2,15 @@
 using namespace std;
 
 int maxSubarrayProduct(int arr[], int n){
-    int maxEH = 1;
-    int minEH = 1;
-    int flag =0;
-    int maxSF = 0;
-    for(int i = 0;i<n;i++){
-        if(arr[i]< 0){
-            int temp = maxEH;
-            maxEH = max(minEH * arr[i], 1);
-            minEH = temp * arr[i];
-        }else
-            if(arr[i] == 0){
-                maxEH = 1;
-                minEH = 1;
-            }
-            else{
-                maxEH = maxEH * arr[i];
-                minEH = min(arr[i]*minEH , 1);
-                flag = 1;
-            }
-        if(maxSF < maxEH) maxSF = maxEH;
-    }
-    if(maxSF == 0 && flag == 0){
-        return 0;
-    }
-    if(maxSF == 1){
-        maxSF = arr[0];
-        for(int i = 1; i< n; i++){
-            maxSF = max( maxSF, arr[i]);
-        }
-    }
+    int maxEH = arr[0];
+    int minEH = arr[0];
+    int maxSF = arr[0];
+    for(int i = 1; i< n; i++){
+        int temp = max({arr[i], arr[i]*maxEH, arr[i]* minEH });
+        minEH = min({arr[i],arr[i]*maxEH,arr[i]*minEH});
+        maxEH =temp;
+        maxSF = max(maxSF, maxEH);
+    }    
     return maxSF;
 }
 
