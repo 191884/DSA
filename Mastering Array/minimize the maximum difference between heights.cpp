b7eb6a30d393f22e1eb7c1eea5f6dd2125f2c648
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-01 21:18:37 +0530

    Minute Change

--- Mastering Array/minimize the maximum difference between heights.cpp
+++ Mastering Array/minimize the maximum difference between heights.cpp
@@ -7,7 +7,7 @@ int solve(int arr[], int n, int k){
 
     int tempMin = arr[0];
     int tempMax = arr[n-1];
-    for (int  i = 0; i < n; i++)
+    for (int  i = 1; i < n; i++)
     {
         if(arr[i] - k < 0) continue;
 
