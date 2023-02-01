a0ed8d6792703f6b683beb404c9228b90e195b79
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-01 21:14:34 +0530

    Minimize the maximum diffenence between heights Done

---
+++ Mastering Array/minimize the maximum difference between heights.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int arr[], int n, int k){
+    sort(arr, arr+n);
+    int ans = arr[n-1] - arr[0];
+
+    int tempMin = arr[0];
+    int tempMax = arr[n-1];
+    for (int  i = 0; i < n; i++)
+    {
+        if(arr[i] - k < 0) continue;
+
+        tempMin = min(arr[0] + k, arr[i] -k);
+        tempMax = max(arr[n-1]-k, arr[i-1] + k);
+        ans = min(ans, tempMax- tempMin);
+    }
+    return ans;
+}
+int main(){
+    int k = 6, n = 6;
+    int arr[n] = { 7, 4, 8, 8, 8, 9 };
+    int ans = solve(arr, n, k);
+    cout<<"The minimum difference we can get is "<<ans;
+}
