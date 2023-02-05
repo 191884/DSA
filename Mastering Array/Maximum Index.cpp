d301c1f5fbbe5abf77c1e9b8262f02667db5a942
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-05 21:56:54 +0530

    Different approach

--- Mastering Array/Maximum Index.cpp
+++ Mastering Array/Maximum Index.cpp
@@ -1,15 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 int solve(int arr[], int n){
-    int maxDiff = -1;
-    for(int i = 0; i< n; ++i){
-        for(int j = n-1; j>= 0; --j){
-            if(arr[j] > arr[i] && maxDiff < (j-i)){
-                maxDiff = j -i;
-            }
+    int leftMin[n] ;
+    leftMin[0] = arr[0];
+    for(int i = 1 ; i<n; i++)
+        leftMin[i] = min(leftMin[i-1], arr[i]);
+ 
+ 
+    //leftMin[i] = min{ arr[0...i] }
+ 
+ 
+    int maxDist = INT_MIN;
+    int i = n-1, j = n-1;
+ 
+    while(i>=0  &&  j>=0)
+    {
+        if(arr[j] >= leftMin[i])
+        {
+            maxDist = max(maxDist, j-i);
+            i--;
         }
+        else
+            j--;
     }
-    return maxDiff;
+ 
+    return maxDist;
 }
 int main(){
     int arr[] = {34, 8, 10, 3, 2, 80, 30, 33, 1};
