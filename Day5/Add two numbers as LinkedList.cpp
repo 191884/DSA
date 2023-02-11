bece8f5baee5674a3b57d5a99389773b3abbc19d
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-11 11:25:52 +0530

    Add two numbers Done.

--- Day5/Add two numbers as LinkedList.cpp
+++ Day5/Add two numbers as LinkedList.cpp
@@ -1,23 +1,80 @@
-ListNode* addTwoNums(ListNode* l1, ListNode* l2){
-    ListNode* dummy = new ListNode();
-    ListNode* temp = dummy;
-    int carry = 0;
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+
+    public:
+    int data;
+    Node* next;
+
+    Node(){
+        this -> data = 0;
+        this -> next = NULL;
+    }
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+
+};
+
+void InsertionAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    tail = temp;
+}
+
+void printLL(Node* &head){
+    Node* temp = head;
+    while (temp != NULL) {
+        cout<<temp -> data<<endl;
+        temp = temp -> next;
+    }
+    
+}
+
+void solve(Node* &l1, Node* &l2, Node* &ans){
+    int sum = 0, carry = 1;
     while(l1 != NULL || l2 != NULL || carry){
-        int sum = 0;
+        sum = 0;
         if(l1 != NULL){
-            sum += l1-> val;
-            l1 = l1->next;
+            sum += l1 -> data;
+            l1 = l1 -> next;
         }
         if(l2 != NULL){
-            sum = l2 ->val;
+            sum += l2 -> data;
             l2 = l2 -> next;
         }
-
         sum += carry;
         carry = sum/10;
-        ListNode* node = new ListNode(sum%10);
-        temp -> next = node;
-        temp = temp; 
-
+        InsertionAtTail(ans, sum%10);
     }
+}
+
+int main(){
+    
+    Node* l1 = new Node(9);
+    Node* tail1 = l1;
+    Node* l2 = new Node(9);
+    Node* tail2 = l2;
+    Node* ans = new Node();
+    Node* tail = ans;
+
+    InsertionAtTail(tail1, 9);
+    InsertionAtTail(tail1, 9);
+    InsertionAtTail(tail1, 9);
+    InsertionAtTail(tail1, 9);
+    InsertionAtTail(tail1, 9);
+    InsertionAtTail(tail1, 9);
+
+    InsertionAtTail(tail2, 9);
+    InsertionAtTail(tail2, 9);
+    InsertionAtTail(tail2, 9);
+
+    solve(l1, l2, ans);
+
+    printLL(tail -> next);
+    
+    return 0;
 }
