f54e8700b86093cdfccccf855e8c637d7841d2d1
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-13 22:17:33 +0530

    Done.

--- Day6/Starting point of a cycle LL.cpp
+++ Day6/Starting point of a cycle LL.cpp
@@ -1,32 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class node {
+class Node {
     public:
-        int num;
-        node* next;
-        node(int val) {
-            num = val;
-            next = NULL;
+        int data;
+        Node* next;
+        Node(int data) {
+            this -> data = data;
+            this -> next = NULL;
         }
 };
 
-void insertNode(node* &head,int val) {
-    node* newNode = new node(val);
+void insertNode(Node* &head,int val) {
+    Node* newNode = new Node(val);
     if(head == NULL) {
         head = newNode;
         return;
     }
-    node* temp = head;
+    Node* temp = head;
     while(temp->next != NULL) temp = temp->next;
     
     temp->next = newNode;
     return;
 }
 
-void createCycle(node* &head,int pos) {
-    node* ptr = head;
-    node* temp = head;
+void createCycle(Node* &head,int pos) {
+    Node* ptr = head;
+    Node* temp = head;
     int cnt = 0;
     while(temp->next != NULL) {
         if(cnt != pos) {
@@ -37,19 +37,29 @@ void createCycle(node* &head,int pos) {
     }
     temp->next = ptr;
 }
-//process as per mentioned in solution
-node* detectCycle(node* head) {
-    unordered_set<node*> st;
-    while(head != NULL) {
-        if(st.find(head) != st.end()) return head;
-        st.insert(head);
-        head = head->next;
+Node* detectCycle(Node* head) {
+    if(head == NULL||head->next == NULL) return NULL;
+        
+    Node* fast = head;
+    Node* slow = head;
+    Node* entry = head;
+        
+    while(fast->next != NULL&&fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+            
+        if(slow == fast) {
+            while(slow != entry) {
+                slow = slow->next;
+                entry = entry->next;
+            }
+            return slow;
+        }
     }
     return NULL;
 }
-
 int main() {
-    node* head = NULL;
+    Node* head = NULL;
     
     insertNode(head,1);
     insertNode(head,2);
@@ -61,10 +71,10 @@ int main() {
     
     createCycle(head,2);
     
-    node* nodeRecieve = detectCycle(head);
+    Node* nodeRecieve = detectCycle(head);
     if(nodeRecieve == NULL) cout<<"No cycle";
     else {
-        node* temp = head;
+        Node* temp = head;
         int pos = 0;
         while(temp!=nodeRecieve) {
             ++pos;
