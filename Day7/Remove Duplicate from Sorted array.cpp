7b80115a8890cc5a6f8345d04c00c0b713052352
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-15 22:31:27 +0530

    Done.

--- Day7/Remove Duplicate from Sorted array.cpp
+++ Day7/Remove Duplicate from Sorted array.cpp
@@ -1,17 +1,16 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-int removeDuplicates(int arr[], int n) {
-  set < int > set;
-  for (int i = 0; i < n; i++) {
-    set.insert(arr[i]);
+int removeDuplicates(int arr[], int n)
+{
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[i] != arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
   }
-  int k = set.size();
-  int j = 0;
-  for (int x: set) {
-    arr[j++] = x;
-  }
-  return k;
+  return i + 1;
 }
 int main() {
   int arr[] = {1,1,2,2,2,3,3};
