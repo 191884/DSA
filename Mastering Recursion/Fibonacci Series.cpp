bbc449ba24edcb91a89e6849f20f57d7e022edfc
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-18 22:19:40 +0530

    Print Fibonacci Series Done.

--- Mastering Recursion/Fibonacci Series.cpp
+++ Mastering Recursion/Fibonacci Series.cpp
@@ -1,12 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fibSeries(int n){
-    if(n <= 1) return n;
-    return fibSeries(n-1) + fibSeries(n-2);
+void fibSeries(int n){
+    static int t1 = 0, t2 = 1;
+    if(n > 0){
+        int nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        cout<<nextTerm<<" ";
+        fibSeries(n-1);
+    }
 }
+
+// int fibSeries(int n){
+//     if(n <= 1) return n;
+//     return fibSeries(n-1) + fibSeries(n-2);
+// }
 
 int main(){
     int n = 9;
-    cout<<fibSeries(n);
+    cout<<"0 1 ";
+    fibSeries(n-2);
 }
