4d3b48a6d230060734f594f98338b51ef68c20bc
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-18 15:40:29 +0530

    Done.

--- Mastering Recursion/Print Array.cpp
+++ Mastering Recursion/Print Array.cpp
@@ -1,14 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printarr(int arr[] ,int n){
+void print1toN(int arr[] ,int n){
     if(n < 0) return;
-    printarr(arr ,n-1);
+    print1toN(arr ,n-1);
     cout<<arr[n]<<" ";
+}
+
+//for reverse printing array
+void printnto1(int arr[], int n){
+    if(n == 0){
+        cout<<arr[n]<<" ";
+        return;
+    }
+    cout<<arr[n]<<" ";
+    printnto1(arr, n-1);
 }
 
 int main(){
     int arr[] = {2, 3, 4, 5, 1};
     int n = 5;
-    printarr(arr, n-1);
+    print1toN(arr, n-1);
+    cout<<endl;
+    printnto1(arr, n-1);
 }
