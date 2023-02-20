565e030f25090944b3ba9cc5730ec96a338d599e
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-20 12:47:31 +0530

    Aditya Verma's PowerSet Solution.

--- Mastering Recursion/PowerSet.cpp
+++ Mastering Recursion/PowerSet.cpp
@@ -12,11 +12,32 @@ void powerSet(vector<int> nums, vector<int> output, int index, vector<vector<int
     powerSet(nums, output, index +1, ans);
 }
 
+//Aditya Verma Solution
+// void powerSet(string ip, string op){
+//     if(ip.length() == 0){
+//         cout<<op<<" ";
+//         return;
+//     }
+//     string op1 = op;
+//     string op2 = op;
+
+//     op2.push_back(ip[0]);
+//     ip.erase(ip.begin() + 0);
+//     powerSet(ip, op1);
+//     powerSet(ip, op2);
+//     return;
+// }
+
 int main(){
     vector<int> nums = {1,2,3};
     vector<vector<int>> ans;
     vector<int> output;
     int index = 0;
+
+    // string ip = "abc";
+    // string op = "";
+    // powerSet(ip, op);
+    
     powerSet(nums, output, index, ans);
     cout<<ans[0].size();
     for(int i = 0; i< ans.size(); i++){
