82b3f74a563884911179d5a963589a270c674297
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-24 09:13:32 +0530

    Combination Sum With repeatition Done.

--- Day9/Combination Sum(repetition alloweded).cpp
+++ Day9/Combination Sum(repetition alloweded).cpp
@@ -2,29 +2,29 @@
 using namespace std;
 
 void solve(vector<int>& nums, vector<int>& output, int index, int target, vector<vector<int>>& ans){
-    if(index == nums.size()){
-        if(target == 0){
-            ans.push_back(output);
-            cout<<ans.size()<<" ";
+     if (index == nums.size()) {
+        if (target == 0) {
+          ans.push_back(output);
         }
         return;
-    }
+      }
 
-    if(nums[index] <= target){
+      if (nums[index] <= target) {
         output.push_back(nums[index]);
         solve(nums, output, index, target - nums[index], ans);
         output.pop_back();
-    }
-    solve(nums,output, index+1, target, ans);
+      }
 
+      solve(nums, output,index + 1, target, ans);
 }
 
 int main(){
     vector<int> nums = {2,3,6,7};
+    int target = 7;
     vector<vector<int>> ans;
     vector<int> output;
     int index = 0;
-    int target = 7;
+    
 
     solve(nums, output, index, target, ans);
     
