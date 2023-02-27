92999086f0aeb901396ae69cb6f447e8f3d77341
Aman Kumar <dr.amannavodian@gmail.com>
2023-02-27 22:07:47 +0530

    Bitonic Sum Done.

--- Mastering Array/Bitonic Point.cpp
+++ Mastering Array/Bitonic Point.cpp
@@ -1,37 +1,30 @@
-// Find a peak element which is not smaller than its neighbours
+#include<bits/stdc++.h>
+using namespace std;
 
 // Naive Approach
-class Solution{
-public:
-	
-	int findMaximum(int arr[], int n) {
-	    for(int i = 1; i<n; i++){
-	        if(arr[i]> arr[i+1]&& arr[i]>arr[i-1]){
-	            return arr[i];
-	        }
-	    }
-	}
-};
+// int findMaximum(int arr[], int n) {
+// 	    for(int i = 1; i<n; i++){
+// 	        if(arr[i]> arr[i+1]&& arr[i]>arr[i-1]){
+// 	            return arr[i];
+// 	        }
+// 	    }
+// }
 
-//Using Binary Search
-
-class Solution{
-public:
-	
-	int findMaximum(int arr[], int n) {
-	    int ans = findMaximumE(arr, 0, n, n);
-	    return ans;
-	}
-	
-	int findMaximumE(int arr[], int low, int high, int n) {
+int findMaximum(int arr[], int low, int high, int n) {
 	    int mid = low + (high - low)/2;
 	    if((mid==0 ||arr[mid] > arr[mid-1]) && (arr[mid] > arr[mid+1]|| mid ==n-1)){
 	        return arr[mid];
 	    }else{
 	         if(arr[mid]<arr[mid-1]&&mid>0){
-	            return findMaximumE(arr, 0, (mid-1), n);
+	            return findMaximum(arr, 0, (mid-1), n);
 	    }   else 
-	            return findMaximumE(arr, (mid+1), high, n);
+	            return findMaximum(arr, (mid+1), high, n);
 	    }
 	}
-};
+
+int main(){
+	int arr[] = {2, 5, 3, 4, 7, 6};
+	int n = sizeof(arr)/ sizeof(arr[0]);
+	int ans = findMaximum(arr, 0, n, n);
+	cout<<ans;
+}
